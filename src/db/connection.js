31f4a9ff40d5d28a5562ede7446ed477f3dbf603
Vitor Wagner <vitormwagner@gmail.com>
2023-03-21 17:46:05 -0300

    fix: :hammer: Environment variable change

--- src/db/connection.js
+++ src/db/connection.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2/promise');
 
 const connection = mysql.createPool({
   host: process.env.MYSQL_HOST,
-  port: process.env.MYSQL_PORT,
+  port: process.env.MYSQL_PORT || 3306,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: 'StoreManager',
