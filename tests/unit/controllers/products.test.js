f770b3bb5af3ee5cac1099b39bd3d89470ae2fe5
Vitor Wagner <vitormwagner@gmail.com>
2023-03-24 15:49:31 -0300

    feat: :sparkles: Adds tests for products controller

--- tests/unit/controllers/products.test.js
+++ tests/unit/controllers/products.test.js
@@ -1,5 +1,6 @@
 const chai = require("chai");
 const sinon = require("sinon");
+const dataMock = require("../../Mocks/DataMock");
 
 chai.use(require("chai-as-promised"));
 chai.use(require("sinon-chai"));
@@ -8,7 +9,6 @@ const { expect } = chai;
 
 const ProductsController = require("../../../src/controllers/products.controllers");
 const ProductsService = require("../../../src/services/products.services");
-const ProductsModel = require("../../../src/models/products.model");
 
 const productsMock = [
   {
@@ -63,8 +63,10 @@ describe("testa se o controller de produtos", () => {
       response.status = sinon.stub().returns(response);
       response.json = sinon.stub().returns();
 
-      sinon.stub(ProductsModel, "getProductById").resolves(null);
-      
+      sinon
+        .stub(ProductsService, "getProductById")
+        .resolves({ type: "NOT_FOUND", message: "Product not found" });
+
       await ProductsController.getProductById(request, response);
 
       expect(response.status).to.have.been.calledWith(404);
@@ -73,6 +75,139 @@ describe("testa se o controller de produtos", () => {
       });
     });
   });
+
+  describe("testa se postProduct", () => {
+    it("retorna um produto com o status correto", async () => {
+      const request = { body: dataMock.rightProductBody };
+      const response = {};
+      response.status = sinon.stub().returns(response);
+      response.json = sinon.stub().returns();
+
+      sinon
+        .stub(ProductsService, "postProduct")
+        .resolves({ type: null, id: 4, message: dataMock.rightProductBody });
+
+      await ProductsController.postProduct(request, response);
+
+      expect(response.status).to.have.been.calledWith(201);
+      expect(response.json).to.have.been.calledWith({
+        id: 4,
+        name: dataMock.rightProductBody,
+      });
+    });
+    it("retorna um erro quando o nome do produto não é informado", async () => {
+      const request = { body: dataMock.wrongProductBody };
+      const response = {};
+      response.status = sinon.stub().returns(response);
+      response.json = sinon.stub().returns();
+
+      sinon
+        .stub(ProductsService, "postProduct")
+        .resolves({
+          type: "NAME_IS_REQUIRED",
+          message: "'name' is required",
+        });
+
+      await ProductsController.postProduct(request, response);
+
+      expect(response.status).to.have.been.calledWith(400);
+      expect(response.json).to.have.been.calledWith({
+        message: "'name' is required",
+      });
+    });
+  });
+
+  describe("testa se searchProductsByName", () => {
+    it("retorna um produto com o status correto", async () => {
+      const [productMock] = productsMock;
+      const request = { query: { q: "Thor" } };
+      const response = {};
+      response.status = sinon.stub().returns(response);
+      response.json = sinon.stub().returns();
+
+      sinon.stub(ProductsService, "searchProductsByName").resolves({ message: productMock });
+
+      await ProductsController.searchProductsByName(request, response);
+
+      expect(response.status).to.have.been.calledWith(200);
+      expect(response.json).to.have.been.calledWith(productMock);
+    });
+  });
+
+  describe("testa se updateProduct", () => {
+    it("retorna um produto com o status correto", async () => {
+      const request = { params: { id: 1 }, body: dataMock.productUpdateBody };
+      const response = {};
+      response.status = sinon.stub().returns(response);
+      response.json = sinon.stub().returns();
+
+      sinon
+        .stub(ProductsService, "updateProduct")
+        .resolves({ type: null, message: dataMock.productUpdateBody.name });
+      
+      await ProductsController.updateProduct(request, response);
+
+      expect(response.status).to.have.been.calledWith(200);
+      expect(response.json).to.have.been.calledWith({
+        id: 1,
+        name: dataMock.productUpdateBody.name,
+      });
+    });
+
+    it("retorna um erro quando não há um produto com este id", async () => {
+      const request = { params: { id: 777 }, body: dataMock.productUpdateBody };
+      const response = {};
+      response.status = sinon.stub().returns(response);
+      response.json = sinon.stub().returns();
+
+      sinon
+        .stub(ProductsService, "updateProduct")
+        .resolves({ type: "NOT_FOUND", message: "Product not found" });
+      
+      await ProductsController.updateProduct(request, response);
+
+      expect(response.status).to.have.been.calledWith(404);
+      expect(response.json).to.have.been.calledWith({
+        message: "Product not found",
+      });
+    });      
+  });
+
+  describe("testa se deleteProduct", () => {
+    it("deleta um produto com o status correto", async () => {
+      const request = { params: { id: 1 } };
+      const response = {};
+      response.status = sinon.stub().returns(response);
+      response.end = sinon.stub().returns();
+
+      sinon.stub(ProductsService, "deleteProduct").resolves({ type: null, message: null });
+
+      await ProductsController.deleteProduct(request, response);
+
+      expect(response.status).to.have.been.calledWith(204);
+      expect(response.end).to.have.been.calledOnce;
+    });
+
+    it("retorna um erro quando não há um produto com este id", async () => {
+      const request = { params: { id: 777 } };
+      const response = {};
+      response.status = sinon.stub().returns(response);
+      response.json = sinon.stub().returns();
+      response.end = sinon.stub().returns();
+
+      sinon
+        .stub(ProductsService, "deleteProduct")
+        .resolves({ type: "NOT_FOUND", message: "Product not found" });
+      
+      await ProductsController.deleteProduct(request, response);
+
+      expect(response.status).to.have.been.calledWith(404);
+      expect(response.json).to.have.been.calledWith({
+        message: "Product not found",
+      });
+    });
+  });
+
 
   afterEach(() => {
     sinon.restore();
